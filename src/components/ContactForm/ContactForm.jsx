e6dbd470cb758d2821b5111b040ded700df4391a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-04 16:59:29 +0300

    Update ContactForm component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,21 @@
+import { Field, Form, Formik } from 'formik';
+import css from './ContactForm.module.css';
+
 const ContactForm = () => {
-  return <div></div>;
+  return (
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      <Form className={css.form}>
+        <label htmlFor="">Name</label>
+        <Field className={css.field} type="text" name="name" id="" />
+
+        <label htmlFor="">Number</label>
+        <Field className={css.field} type="text" name="number" id="" />
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
