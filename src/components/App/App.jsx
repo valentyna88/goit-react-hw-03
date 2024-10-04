7ccb2831725b0b0f15c8fa1e43bccd0bbb533b78
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-04 16:29:11 +0300

    Initial components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,18 @@
 import './App.module.css';
 
+import ContactForm from '../ContactForm/ContactForm';
+import SearchBox from '../SearchBox/SearchBox';
+import ContactList from '../ContactList/ContactList';
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,5 @@
+const SearchBox = () => {
+  return <div></div>;
+};
+
+export default SearchBox;
