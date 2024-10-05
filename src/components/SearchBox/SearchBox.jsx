439b9dd54643cceeec122c502a62ad38909e8842
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-05 09:04:43 +0300

    Add SearchBox component markup and styles

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,12 @@
+import css from './SearchBox.module.css';
+
 const SearchBox = () => {
-  return <div></div>;
+  return (
+    <div className={css.wrapper}>
+      <label htmlFor="">Find contacts by name</label>
+      <input className={css.field} type="text" />
+    </div>
+  );
 };
 
 export default SearchBox;
